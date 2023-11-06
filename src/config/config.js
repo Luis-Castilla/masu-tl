49886ad3cee00bdaf57e4ec3d1ccecdeca6d4b41
ext_lgcastillas <ext_lgcastillas@falabella.cl>
2023-11-06 12:04:05 -0500

    get product by id, JSDoc

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,16 @@
-//
+/**
+ * Module providing configuration for the Dummy Data API URL.
+ * @module dummyConfig
+ */
+
+/**
+ * Configuration object for the Dummy Data API URL.
+ * @property {string} url - The URL of the Dummy Data API.
+ */
+const dummyData = {
+  url: process.env.DUMMY_DATA_URL || 'https://dummyjson.com/products'
+};
+
+module.exports = {
+  dummyData
+};
---
+++ src/utils/error-handler.js
@@ -0,0 +1,30 @@
+/**
+ * Class representing a custom error with an HTTP status code.
+ * @class
+ * @extends Error
+ */
+class CustomError extends Error {
+  /**
+   * Creates an instance of CustomError.
+   * @constructor
+   * @param {number} code - The HTTP status code of the error.
+   * @param {string} message - The error message.
+   */
+  constructor(code, message) {
+    super(message);
+
+    /**
+     * Name of the error class.
+     * @member {string}
+     */
+    this.name = 'CustomError';
+
+    /**
+     * HTTP status code of the error.
+     * @member {number}
+     */
+    this.status = code;
+  }
+}
+
+module.exports = CustomError;
